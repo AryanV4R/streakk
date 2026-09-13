3ee838bc4b8134d28cb6d5a915e449655fb0573a
AryanV4R <idabhinavx@protonmail.com>
2026-09-13 20:18:26 +0530

    Add androidx.fragment:fragment dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
+    implementation("androidx.fragment:fragment:1.8.6")
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
